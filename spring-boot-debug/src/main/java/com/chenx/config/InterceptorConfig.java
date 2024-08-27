0cecaed9d0532d344ebbbccc923afc034f62bb38
chenxing <1057791979@qq.com>
2024-08-27 14:37:56 +0800

    添加一个拦截器用于调试

---
+++ spring-boot-debug/src/main/java/com/chenx/config/InterceptorConfig.java
@@ -0,0 +1,27 @@
+package com.chenx.config;
+
+import com.chenx.interceptor.DemoInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+	@Autowired
+	private DemoInterceptor interceptor;
+
+	/**
+	 * 添加一个全路径拦截器，用于测试{@link DispatcherServlet#doDispatch(HttpServletRequest, HttpServletResponse)} 中getHandler返回值是否包含拦截器
+	 * @param registry
+	 */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(interceptor)
+				.addPathPatterns("/**");
+	}
+}
--- spring-boot-debug/src/main/java/com/chenx/controller/DemoController.java
+++ spring-boot-debug/src/main/java/com/chenx/controller/DemoController.java
@@ -1,19 +1,43 @@
 package com.chenx.controller;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @RestController
-public class DemoController {
+public class DemoController implements ApplicationContextAware {
 	// DispatcherServlet实例也是作为单例放在单例池中的
 	@Autowired
 	private DispatcherServlet servlet;
 
+	private ApplicationContext applicationContext;
+
+	@Autowired
+	private DemoController demoController;
+
 	@GetMapping("/success")
 	public String getInfo() {
 		System.out.println(servlet);
+		// spring mvc和我们使用的是同一个IOC容器
+		System.out.println(applicationContext);
 		return "Success!!!";
+	}
+
+	/**
+	 * 因为springmvc的配置，/**路径处理器，会被配置为默认处理器，如果一个请求没有找到对应的路径处理器，就会使用默认处理器
+	 */
+	@RequestMapping("/**")
+	public String defaultHandler(){
+		return "default Handler handle this request";
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
 	}
 }
