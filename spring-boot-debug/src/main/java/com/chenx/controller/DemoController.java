c906a278102ce30129a245f2ce0da93abf034fdb
chenxing <1057791979@qq.com>
2024-08-30 00:17:33 +0800

    springboot和springmvc共用一个ioc容器

--- spring-boot-debug/src/main/java/com/chenx/controller/DemoController.java
+++ spring-boot-debug/src/main/java/com/chenx/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.chenx.controller;
 
+import com.chenx.service.DemoService;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,9 @@ public class DemoController implements ApplicationContextAware, InitializingBean
 	@Autowired
 	private DispatcherServlet servlet;
 
+	@Autowired
+	private DemoService demoService;
+
 	private ApplicationContext applicationContext;
 
 	@Autowired
@@ -26,10 +30,19 @@ public class DemoController implements ApplicationContextAware, InitializingBean
 	@GetMapping("/success")
 	public String getInfo() {
 		System.out.println(servlet);
-		// spring mvc和我们使用的是同一个IOC容器
+		// 在springboot中，spring mvc和我们使用的是同一个IOC容器
+		// 如果是spring + spring mvc的项目中，spring和springmvc各自持有一个ioc容器
+		// 且spring的容器是mvc容器的父容器，所以mvc可以访问spring容器中的内容，而spring不能访问mvc容器中的内容
+		// 所以spring+springmvc项目中，spring容器中可能没有Controller对应的bean（道听途说 如下）
+		/*
+		有一篇博客说，Spring MVC和Spring本身用的不是一个IoC容器，Spring 的IoC容器是MVC的IoC容器的父容器
+	    所以MVC可以访问Spring的IoC容器，而Spring不能访问MVC的IoC容器，结果就是Controller可以访问Service对象，
+	    而Service对象不能访问Controller对象
+		 */
 		System.out.println(applicationContext);
 		return "Success!!!";
 	}
+
 
 	/**
 	 * 因为springmvc的配置，/**路径处理器，会被配置为默认处理器，如果一个请求没有找到对应的路径处理器，就会使用默认处理器
